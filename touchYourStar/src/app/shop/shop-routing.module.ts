51bd46959df5018679b66bcb7bf834554a949ea9
cyrilprudhomme <71281968+cyrilprudhomme@users.noreply.github.com>
2021-01-13 19:31:22 +0100

    Add detailProducts

--- touchYourStar/src/app/shop/shop-routing.module.ts
+++ touchYourStar/src/app/shop/shop-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailProductsComponent } from './components/detail-products/detail-products.component';
 import { ShopComponent } from './shop.component';
 
 const routes: Routes = [
   { path: '', component: ShopComponent },
+  { path: 'detail/:id', component: DetailProductsComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
