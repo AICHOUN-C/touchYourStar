14937472aced99c1284c9412846c5c41fa41018b
AICHOUN-C <cherif.aichoun@gmail.com>
2021-01-14 10:54:28 +0100

    fix: simplify data

---
+++ api/src/main/java/com/hackathon3/api/dto/ProductDto.java
@@ -0,0 +1,51 @@
+package com.hackathon3.api.dto;
+
+
+public class ProductDto {
+	
+	private String category;
+	private String price;
+	private int quantity;
+	private String name;
+	private String description;
+	private String reference;
+	
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	
+}
---
+++ api/src/main/java/com/hackathon3/api/services/ProductService.java
@@ -0,0 +1,40 @@
+package com.hackathon3.api.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hackathon3.api.dto.ProductDto;
+import com.hackathon3.api.entities.Product;
+import com.hackathon3.api.mappers.ProductMapper;
+import com.hackathon3.api.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	@Autowired
+	private ProductMapper productMapper;
+	
+	public List<Product> findAllProducts() {
+		return productRepository.findAll();
+	}
+	
+	public Product findProductById(Long id) {
+		return productRepository.findById(id).orElse(null);
+	}
+	
+	public Product update(Long id, ProductDto dto) {
+		Product product = productRepository.findById(id).orElse(null);
+		product = productMapper.update(product, dto);
+		return productRepository.save(product);
+	}
+	
+	public void delete (Long id) {
+		Optional<Product> productOptional = productRepository.findById(id);
+		productOptional.ifPresent(product -> productRepository.delete(product));
+	}
+}
