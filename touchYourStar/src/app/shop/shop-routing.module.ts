81791dbf2bf6c63c5f192cff7ed748a69eb24f4e
mickaelBell <mickael.belleuvre@gmail.com>
2021-01-15 03:00:50 +0100

    meta css modify

--- touchYourStar/src/app/shop/shop-routing.module.ts
+++ touchYourStar/src/app/shop/shop-routing.module.ts
@@ -7,8 +7,8 @@ import { ShopComponent } from './shop.component';
 
 const routes: Routes = [
   { path: 'list', component: ShopComponent, data:{ title: "Touched By Star", description: "page d'acceuil" } },
-  { path: 'list/:detailId', component: DetailProductsComponent, data:{ title: "trotoir", description: 'erreur 404' } },
-  { path: 'order', component: OrderShopComponent, data:{ title: "ce n'est pas par ici", description: 'erreur 404' }},
+  { path: 'list/:detailId', component: DetailProductsComponent, data:{ title: "Votre objet Touched By Star", description: 'détail sur votre objet sélectionné' } },
+  { path: 'order', component: OrderShopComponent, data:{ title: "Votre commande de Star", description: 'détail de votre commande sur Touched By Star' }},
   { path: '', redirectTo: 'list', pathMatch: 'full' },
 ];
 
